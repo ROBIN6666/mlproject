6649d0cde68abcb9c360ca508967f887e693a2ce
Rahul <rahul.kumark72@gmail.com>
2023-05-12 01:54:44 +0530

    added data_ingestion_pipeline

--- src/logger.py
+++ src/logger.py
@@ -2,17 +2,15 @@ import logging
 import os
 from datetime import datetime
 
-#The format of the log file will be like this
-LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-log_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
-#Keep on appending the files and folder inside it
-os.makedirs(log_path,exist_ok=True)
-LOG_FILE_PATH=os.path.join(log_path,LOG_FILE)
+# The format of the log file will be like this
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+log_path = os.path.join(os.getcwd(), "logs", LOG_FILE)
+# Keep on appending the files and folder inside it
+os.makedirs(log_path, exist_ok=True)
+LOG_FILE_PATH = os.path.join(log_path, LOG_FILE)
 
 logging.basicConfig(
     filename=LOG_FILE_PATH,
     format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s -%(message)s",
     level=logging.INFO,
 )
-
-
